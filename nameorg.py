a9f3663335bda6475e985547a1653fb74e8bd788
Arnav Shah <arnavshah@Arnavs-Laptop.local>
2021-06-23 00:05:15 -0700

    updated fully working

--- nameorg.py
+++ nameorg.py
@@ -6,6 +6,7 @@ class Organization:
         self.makedict()
 
     def formatnames(self, filename):
+        # formats names from a list in a txt file
         with open(filename) as f:
             a = f.readlines()
             self.namelist = []
@@ -15,16 +16,18 @@ class Organization:
         return self.namelist
 
     def formatlinks(self, filename):
+        # formats links from a list in a txt file
         with open(filename) as f:
             a = f.readlines()
             self.linklist = []
             for eachline in a:
                 eachline = eachline.strip()
-                eachline = 'https://www.' + eachline
+                eachline = 'http://' + eachline
                 self.linklist.append(eachline)
         return self.linklist
 
     def makedict(self):
+        # makes a dictionary with names + links
         assert len(self.namelist) == len(self.linklist)
         self.dic = {}
         for index in range(len(self.namelist)):
--- websitess.py
+++ websitess.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 import nameorg
 import time
+from PIL import Image
 
 
 class WebsiteSS:
@@ -8,16 +10,27 @@ class WebsiteSS:
         a = nameorg.Organization()
         self.activedic = a.makedict()
 
-    def go_to_website(self): 
+    def go_to_website(self):
         self.driver = webdriver.Chrome('/Users/arnavshah/Desktop/py/websiteSS/chromedriver')
+        brokenlinks = []
         for name, link in self.activedic.items():
-            self.driver.get(link)
-            time.sleep(3)
-            self.driver.get_screenshot_as_file(f'{name}.png')
-
-
-
-
+            try:
+                self.driver.get(link)
+                time.sleep(3)
+                # image = ImageGrab.grab(bbox=(0, 0, 907, 512))
+                self.driver.get_screenshot_as_file(f'{name}.png')
+                im = Image.open(f'{name}.png')
+                im = im.resize((907, 512))
+                im.save(f'{name}.png')
+            except WebDriverException:
+                link = link.replace('http://', '')
+                try:
+                    self.driver.get(link)
+                    time.sleep(3)
+                    self.driver.get_screenshot_as_file(f'{name}.png')
+                except WebDriverException:
+                    brokenlinks.append(name)
+        print(brokenlinks)
 
 
 w = WebsiteSS()
